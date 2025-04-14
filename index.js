e990f27b71f9058f48d2df7dc876cf75f8dc7bb4
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-14 05:19:40 +0500

    GET /, POST /, PATCH /, DELETE /

--- index.js
+++ index.js
@@ -20,11 +20,24 @@ app.get('/users', (req,res)=>{
 app.get('/api/users', (req,res)=>{
     return res.json(users)
 })
-app.get('/api/users/:id', (req,res)=>{
+
+app
+.route('/api/users/:id')
+.get('/api/users/:id', (req,res)=>{
     const id = Number(req.params.id)
     const user = users.find((user)=>user.id === id)
     return res.json(user)
 })
+.post('/api/users', (req,res)=>{
+    //Create new user
+    return res.json({status: 'pending'})
+})
+.patch('/api/users/:id', (req,res)=>{
+    //Edit user with id
+    return res.json({status: 'pending'})
+})
+
+
 
 
 
